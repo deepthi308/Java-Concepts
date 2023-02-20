65bc910d6ca1277a280309b8066811195b110a8d
Deepthi_Hariraman <73683302+deepthi308@users.noreply.github.com>
2023-02-20 08:48:07 +0530

    Java Concept

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+package javaConcepts;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+              //		Comment ---> Documentation purpose	
+             //		code goes here ---> Single Line Comment
+			/*This 
+			 * is
+			 * a
+			 * Multi Line
+			 * comment*/
+		
+	 System.out.print("Hello World");
+	 
+	 //	 System --> Class
+	 //Out --> Object
+	 //print --> Method
+	 
+		
+	}
+	
+}
