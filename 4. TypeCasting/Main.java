7109b1cfaff3fda4853876f41ca0bbb1587f7495
Deepthi_Hariraman <73683302+deepthi308@users.noreply.github.com>
2023-02-22 11:54:19 +0530

    Java-Concept

---
+++ 4. TypeCasting/Main.java
@@ -0,0 +1,34 @@
+package typecasting;
+
+public class Main {
+
+	
+
+	public static void main(String[] args) {
+		
+		String day = "Monday"; //Local Variable (Inside a method or function)
+		
+		Person person1 = new Person();//Instance of Person
+		Person person2 = new Person();//Instance of Person
+		Person person3 = new Person();//Instance of Person
+		Person person4 = new Person();//Instance of Person
+		Person person5 = new Person();//Instance of Person
+		
+		System.out.println(person1.population);
+		System.out.println(person1.name);
+		person1.setName("Deepthi", 21);
+		System.out.println(person1.name);
+		System.out.println(person1.age);
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+	
--- 4. TypeCasting/Person.java
+++ 4. TypeCasting/Person.java
@@ -1,1 +1,20 @@
+package typecasting;
+
+public class Person{
+	
+	static int population = 0; //Static variable(Common to all instances)
+	static String nationality = "Indian";//Static variable(Common to all instances)
+	
+	 String name; //Instance variables(Specific to that instance inside the class & outside the method)
+	 int age; //Instance variables(Specific to that instance)
+	 
+	 public Person() {
+		 population += 1;
+	 }
+	 
+	public void setName(String name, int  age) {
+		this.name = name;
+		this.age = age;
+	}
+}
 
