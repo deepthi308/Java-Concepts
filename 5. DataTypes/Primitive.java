ced0aea2460e90ed0a649940dfad966c2ae0d3a0
Deepthi_Hariraman <73683302+deepthi308@users.noreply.github.com>
2023-02-22 16:11:18 +0530

    Java-Concept

---
+++ 5. DataTypes/Primitive.java
@@ -0,0 +1,54 @@
+package datatypes;
+
+public class Primitive {
+
+	public static void main(String[] args) {
+		
+//		There are two types of datatypes
+//		1. Primitive Datatypes
+//		2. Non-Primitive Datatypes
+		
+//		Primitive Datatypes --> These are some of the basic datatypes built into java programming language
+		
+		//There are 8 types of primitive datatypes present in java.
+		//They are.
+		/*byte
+		 * short
+		 * int
+		 * long
+		 * float
+		 * double
+		 * boolean
+		 * char*/
+		
+//		DataType                      Default Value        Default Size
+//		boolean	                           false      	   1 bit
+//		char	                             '\u0000'	       2 byte
+//		byte	                              0	             1 byte
+//		short	                              0	             2 byte
+//		int	                                0	             4 byte
+//		long	                              0L	           8 byte
+//		float	                              0.0f	         4 byte
+//		double	                            0.0d	         8 byte
+		
+		byte g = 100;
+		short h = 2000;
+		int a = 30000000;
+		long b = 40000000000000000L;
+		float c = 3.45525F;
+		double d = 3.6837443626;
+		boolean e = false;
+		char f = 'H';
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+
+	}
+
+}
